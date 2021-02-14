242b6a812036d8223cef55a2df77efff85532ffc
Mr.HaCkEr <59619257+mishabzzz@users.noreply.github.com>
2021-02-14 18:28:07 +0000

    Update hack.sh

--- hack.sh
+++ hack.sh
@@ -2,15 +2,12 @@ echo "Hi"
 sleep 1
 
 echo "This is a os_command Injection CTF by H1DD3N_R47"
-sleep 3
+sleep 5
 
 apt install php
 clear
 echo "CTF server Starting..."
 sleep 2
 echo "CTF server Started On (http://localhost:8888/os_commandi.php)"
-echo '''CTF OS COMMAND Injection
-
-
-'''
-php -S localhost:8888/os_commandi.php
+echo '''CTF OS COMMAND Injection'''
+php -S localhost:8888
